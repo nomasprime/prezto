6a63ae3a74e2e1e4245ec28404aa361ba156faba
Rick Jones <rick.jones@playtimestudios.com>
2020-10-10 00:34:37 +0100

    Change list commands time format to long ISO

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -113,17 +113,16 @@ else
   fi
 fi
 
-alias l='ls -1A'         # Lists in one column, hidden files.
-alias ll='ls -lh'        # Lists human readable sizes.
-alias lr='ll -R'         # Lists human readable sizes, recursively.
-alias la='ll -A'         # Lists human readable sizes, hidden files.
-alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
-alias lx='ll -XB'        # Lists sorted by extension (GNU only).
-alias lk='ll -Sr'        # Lists sorted by size, largest last.
-alias lt='ll -tr'        # Lists sorted by date, most recent last.
-alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
-alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
-alias sl='ls'            # I often screw this up.
+alias l='ls -1A'                                # Lists in one column, hidden files.
+alias ll='ls -lh --time-style=long-iso'        # Lists human readable sizes.
+alias lr='ll -R --time-style=long-iso'         # Lists human readable sizes, recursively.
+alias la='ll -A --time-style=long-iso'         # Lists human readable sizes, hidden files.
+alias lm='la | "$PAGER" --time-style=long-iso' # Lists human readable sizes, hidden files through pager.
+alias lx='ll -XB --time-style=long-iso'        # Lists sorted by extension (GNU only).
+alias lk='ll -Sr --time-style=long-iso'        # Lists sorted by size, largest last.
+alias lt='ll -tr --time-style=long-iso'        # Lists sorted by date, most recent last.
+alias lc='lt -c --time-style=long-iso'         # Lists sorted by date, most recent last, shows change time.
+alias lu='lt -u --time-style=long-iso'         # Lists sorted by date, most recent last, shows access time.
 
 # Grep
 if zstyle -t ':prezto:module:utility:grep' color; then
